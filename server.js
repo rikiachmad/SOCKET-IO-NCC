2b4bf7e56dc341659a98e8d0fe101e93c6160979
rikiachmad <rikiachmad@yahoo.com>
2021-03-07 14:50:22 +0700

    bug kecil

--- server.js
+++ server.js
@@ -5,6 +5,9 @@ const port = process.env.PORT || 3000;
 const io = require("socket.io")(server);
 const { joinUser, removeUser } = require("./public/users");
 
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/public/index.html');
+});
 app.use(express.static(__dirname + "/public"));
 server.listen(3000, () => {
   console.log(`Server running at http://127.0.0.1:${port}/`);
